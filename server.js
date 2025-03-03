b862bf044a170edb7a601fae15410afb8d08d992
trzyzielonemyszy <olga.grochala@gmail.com>
2025-03-03 22:38:46 +0100

    time zone fix

--- server.js
+++ server.js
@@ -2,4 +2,10 @@ app.use((req, res, next) => {
   // Ensure dates are handled in Warsaw timezone
   process.env.TZ = 'Europe/Warsaw';
   next();
+});
+
+// Sprawdźmy, na jakim porcie nasłuchuje aplikacja
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 }); 
